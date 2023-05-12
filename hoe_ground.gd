7682c7ec7965669f0909294baa829fedfab53f40
Belal <bmeshaal@bnn.de>
2023-05-12 12:44:03 +0200

    tileset, player and movment, hoe ground

---
+++ hoe_ground.gd
@@ -0,0 +1,10 @@
+extends TileMap
+func _physics_process(_delta):
+	if(Input.is_action_just_pressed("mb_left")):
+		var tile : Vector2 = local_to_map(get_global_mouse_position())
+		var tile_atlas_coords = get_cell_atlas_coords(0,tile)
+		if(tile_atlas_coords.x > 2 && tile_atlas_coords.y == 0):
+			return
+		var random = RandomNumberGenerator.new()
+		random.randomize()
+		set_cell(0,tile,0,Vector2(random.randi_range(3,4),0))
---
+++ modules/player/player-movment.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var speed = 50
+
+func _physics_process(_delta):
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	move_and_slide()
