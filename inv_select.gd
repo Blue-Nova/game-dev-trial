ee6fb7aa8606fac71e2347b06f4ab8a007522123
Belal <bmeshaal@bnn.de>
2023-06-01 12:21:54 +0200

    inventory scrolling with scroll wheel works

--- inv_select.gd
+++ inv_select.gd
@@ -5,38 +5,23 @@ extends HBoxContainer
 
 var selected_index = 0;
 
-func _ready():
-	print(array)
-	print(array[0])
-	print(array[1])
-	print(array[2])
-	print(array[3])
-	print(array[4])
-
 func _physics_process(delta):
-	if Input.is_action_just_pressed("inventory_up"):
+	if Input.is_action_just_released("inventory_up"):
 		print("up")
-		select_up()
-	if Input.is_action_just_pressed("inventory_down"):
+		if selected_index >= array.size()-1:
+			selected_index = array.size()-1
+			return
+		selected_index += 1
+	
+	if Input.is_action_just_released("inventory_down"):
 		print("down")
-		select_down()
-
-func select_up():
-	if selected_index >= array.size()-1:
-		selected_index = array.size()-1
-		return
-	selected_index += 1
-	update_selected()
-
-func select_down():
-	if selected_index <= 0:
-		selected_index = 0
-		return
-	selected_index -= 1
+		if selected_index <= 0:
+			selected_index = 0
+			return
+		selected_index -= 1
+		
 	update_selected()
 
 func update_selected():
 	selected_texture.get_parent().remove_child(selected_texture)
 	array[selected_index].add_child(selected_texture)
-	print(selected_index)
-	print(selected_texture.get_parent())
